914b6af0ff497c5a1134931d3ce2c83a4b7f9f78
Csaba Molnar <docforloop@proton.me>
2025-01-13 17:01:38 +0100

    Add mobile style for the card

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
 import './scss/style.scss';
+
+const button = document.getElementById("share-btn");
+const shareOptions = document.getElementById("share-options");
+const activeButton = document.getElementById("share-btn--active");
+const footer = document.querySelector(".article__footer");
+
+button.addEventListener("click", () => {
+    shareOptions.classList.toggle("show");
+    footer.classList.toggle("hide");
+});
+
+activeButton.addEventListener("click", () => {
+    shareOptions.classList.toggle("show");
+    footer.classList.toggle("hide");
+});
