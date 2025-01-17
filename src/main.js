c66f570ef14148bc4d67ffee921a484ddaa1c6da
Csaba Molnar <docforloop@proton.me>
2025-01-17 11:44:00 +0100

    Refactor js logic

--- src/main.js
+++ src/main.js
@@ -1,54 +1,37 @@
 import './scss/style.scss';
 
 const button = document.getElementById("share-btn");
-const shareOptions = document.getElementById("share-options");
 const activeButton = document.getElementById("share-btn--active");
+const shareOptions = document.getElementById("share-options");
 const footer = document.querySelector(".article__footer");
+
 const isMobile = () => window.matchMedia("(max-width: 808px)").matches;
 
-button.addEventListener("click", () => {
+const toggleShareOptions = () => {
+    shareOptions.classList.toggle("show");
     if(isMobile()) {
-        shareOptions.classList.toggle("show");
-        footer.classList.toggle("hide");
-    }else {
-        shareOptions.classList.toggle("show");
-    }
-    
-});
-
-activeButton.addEventListener("click", () => {
-    if(isMobile()) {
-        shareOptions.classList.toggle("show");
         footer.classList.toggle("hide");
     }
-});
+}
 
-window.addEventListener("resize", () => {
-    if (isMobile()) {
-        shareOptions.classList.remove("show");
-        footer.classList.remove("hide");
-    } else {
-        shareOptions.classList.remove("show");
-        footer.classList.remove("hide");
-    }
-});
+const removeShareOptions = () => {
+    shareOptions.classList.remove("show");
+    footer.classList.remove("hide");
+}
+
+button.addEventListener("click", toggleShareOptions);
+activeButton.addEventListener("click", toggleShareOptions);
+
+window.addEventListener("resize", removeShareOptions);
 
 document.addEventListener("click", (e) => {
-    if (
-        !button.contains(e.target) && !shareOptions.contains(e.target)
-    ) {
-        shareOptions.classList.remove("show");
-        if (isMobile()) {
-            footer.classList.remove("hide");
-        }
+    if (!button.contains(e.target) && !shareOptions.contains(e.target)) {
+        removeShareOptions();
     }
 });
 
 document.addEventListener("keydown", (e) => {
     if (e.key === "Escape") {
-        shareOptions.classList.remove("show");
-        if  (isMobile()) {
-            footer.classList.remove("hide");
-        }
+        removeShareOptions();
     }
 });
